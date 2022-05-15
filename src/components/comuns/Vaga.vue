b23d9e6d5fe7370ecc777f2b0d46066bb6c75081
Gabriel Henrique <bielriquecustodio@gmail.com>
2022-05-15 12:09:37 -0300

    This is my commit to create the Publish Jobs area.  Registration data saved using LocalStorage and redeeming its results to present in the Open Vacancies area.

--- src/components/comuns/Vaga.vue
+++ src/components/comuns/Vaga.vue
@@ -5,7 +5,7 @@
             <p>{{descricao}}</p>
         </div>
         <div class="card-footer">
-            <small class="text-muted">Salário: R$ {{salario}} | Modalidade: {{modalidade}} | Tipo: {{tipo}} | Publicação: {{publicacao}}</small>
+            <small class="text-muted">Salário: R$ {{salario}} | Modalidade: {{getModalidade}} | Tipo: {{getTipo}} | Publicação: {{getPublicacao}}</small>
         </div>
     </div>
 </template>
@@ -43,6 +43,26 @@ export default {
         publicacao: {
             type: String,
             required: true
+        }
+    },
+    computed: {
+        getModalidade() {
+            switch(this.modalidade) {
+                case '1': return 'Home Office'
+                case '2': return 'Presencial'
+            }
+            return ''
+        },
+        getTipo() {
+            switch(this.tipo) {
+                case '1': return 'CLT'
+                case '2': return 'PJ'
+            }
+            return ''
+        },
+        getPublicacao() {
+            let dataPublicacao = new Date(this.publicacao)
+            return dataPublicacao.toLocaleDateString('pt-BR')
         }
     }
 }
